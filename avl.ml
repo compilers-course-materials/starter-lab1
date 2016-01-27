686ab6e7f2c593c4901718a7ce83a8e471212072
Joe Gibbs Politz <joe.politz@gmail.com>
2016-01-27 11:16:16 -0500

    .

---
+++ avl.ml
@@ -0,0 +1,48 @@
+
+
+type ('k, 'v) avlnode =
+  | Leaf
+  | Node of int * 'k * 'v * ('k, 'v) avlnode * ('k, 'v) avlnode
+
+let rec get (n : ('k, 'v) avlnode) (key : 'k) : 'v option =
+  match n with
+    | Leaf -> None
+    | Node(_, k, v, left, right) ->
+      if k = key then Some(v)
+      else if key < k then (get left key)
+      else (get right key)
+
+(* A useful helper function (you may need to write more) *)
+let balance (n : ('k, 'v) avlnode) : ('k, 'v) avlnode =
+  Leaf
+
+(* Produce a new avltree that contains the given key.  If the key already
+   exists, update the value to the new value *)
+let rec set (n : ('k, 'v) avlnode) (key : 'k) (value : 'v) : ('k, 'v) avlnode =
+  Leaf
+
+(* Return a list of tuples containing the elements of the tree *)
+let rec inorder (n : ('k, 'v) avlnode) : ('k * 'v) list =
+  []
+
+
+
+(* Write the functions below (contains, height, add_all, sum) without using
+   any recursion.  Use the functions above, and map/filter/fold, to build up
+   the behavior from existing functions. *)
+
+let contains (n : ('k, 'v) avlnode) (key : 'k) : bool = 
+  false
+
+let height (n : ('k, 'v) avlnode) : int =
+  0
+
+(* set a list of key/value pairs, input as tuples, in the tree *)
+let add_all (n : ('k, 'v) avlnode) (keys : ('k * 'v) list) : ('k, 'v) avlnode =
+  Leaf
+
+(* Return the total value of all the integers in a tree that has int values *)
+let sum (n : ('k, int) avlnode) : int =
+  0
+
+
---
+++ test.ml
@@ -0,0 +1,38 @@
+open OUnit2
+open Avl
+open Expr
+open ExtLib
+
+(* a helper for testing primitive values (won't print datatypes well) *)
+let t_any name value expected = name>::
+  (fun _ -> assert_equal expected value ~printer:dump);;
+
+
+let a_tree = Node(0, "a", 5, Leaf, Leaf);;
+
+(* It can be useful to aggregate tests into lists if they test separate
+functions, and put them together at the end *)
+
+let get_tests = [
+  t_any "get1" (get a_tree "a") (Some(5));
+  t_any "get2" (get (Node(0, "b", 15, a_tree, Leaf)) "a") (Some(5));
+  t_any "get3" (get (Node(0, "b", 15, a_tree, Leaf)) "c") None;
+];;
+
+let contains_tests = [
+  t_any "contains1" (contains a_tree "c") false;
+];;
+
+let evaluate_tests = [
+  t_any "evaluate1" (evaluate (Times(Num(0), Num(5))) Leaf) 0
+]
+
+let all_tests =
+  get_tests @
+  contains_tests @
+  evaluate_tests
+;;
+
+let suite = "suite">:::all_tests;;
+
+run_test_tt_main suite
